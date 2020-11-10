81740f859b2b23de07f1f0e70b64159e5132e0d8
tweltar <sulatttweltarbo16@gmail.com>
2020-11-11 00:48:21 +0630

    Use react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Route, Switch, HashRouter as Router } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+        <Switch>
+          <Route exact path="/home" component={HomePage} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
