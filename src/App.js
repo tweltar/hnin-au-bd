e90f6efe7d51fe0fa19a91cbaf75b71505048e70
tweltar <sulatttweltarbo16@gmail.com>
2020-11-11 21:21:43 +0630

    Add routes for pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 import { Route, Switch, HashRouter as Router } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Wishes from "./pages/Wishes";
+import Memos from "./pages/Memos";
+import Presents from "./pages/Presents";
+import ForYou from "./pages/ForYou";
 import Menu from "./components/Menu";
+import BTSSpoilers from "./pages/BTS&Spoilers";
+// import BTSSpoilers from "./components/BTS&Spoilers";
 
 function App() {
   return (
@@ -13,6 +19,21 @@ function App() {
         </Switch>
         <Switch>
           <Route exact path="/home" component={HomePage} />
+        </Switch>
+        <Switch>
+          <Route exact path="/wishes" component={Wishes} />
+        </Switch>
+        <Switch>
+          <Route exact path="/memos" component={Memos} />
+        </Switch>
+        <Switch>
+          <Route exact path="/presents" component={Presents} />
+        </Switch>
+        <Switch>
+          <Route exact path="/foryou" component={ForYou} />
+        </Switch>
+        <Switch>
+          <Route exact path="/bts&spoilers" component={BTSSpoilers} />
         </Switch>
       </Router>
     </div>
