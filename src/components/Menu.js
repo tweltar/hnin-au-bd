47fc6881d5a60bc8ebfcd6601e4de539138ec3c7
tweltar <sulatttweltarbo16@gmail.com>
2020-11-11 21:22:18 +0630

    Change path name

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -131,7 +131,7 @@ const Menu = () => {
             <ListItemText primary="For You" />
           </ListItem>
         </Link>
-        <Link to="/bts">
+        <Link to="/bts&spoilers">
           <ListItem button>
             <svg
               width="1em"
@@ -146,7 +146,7 @@ const Menu = () => {
                 d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm4 0h8v6H4V1zm8 8H4v6h8V9zM1 1h2v2H1V1zm2 3H1v2h2V4zM1 7h2v2H1V7zm2 3H1v2h2v-2zm-2 3h2v2H1v-2zM15 1h-2v2h2V1zm-2 3h2v2h-2V4zm2 3h-2v2h2V7zm-2 3h2v2h-2v-2zm2 3h-2v2h2v-2z"
               />
             </svg>{" "}
-            <ListItemText primary="BTS" />
+            <ListItemText primary="BTS & Spoilers" />
           </ListItem>
         </Link>
       </List>
