17b8f3ea7f1b85fafcdfe239aea82b2464f7299c
tweltar <sulatttweltarbo16@gmail.com>
2020-11-13 02:40:08 +0630

    Add Spoiler Page

---
+++ src/pages/Spoiler.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spoiler = () => {
+  return (
+    <div className="Page">
+      <h1 className="Page-Title">Behind the Scene & Spoilers</h1>
+    </div>
+  );
+};
+
+export default Spoiler;
