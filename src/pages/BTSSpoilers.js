ceb47f05777662746d1c77823927e22511f6e915
tweltar <sulatttweltarbo16@gmail.com>
2020-11-13 01:40:12 +0630

    Add bts and spoiler page

---
+++ src/pages/BTSSpoilers.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BTSSpoilers = () => {
+  return (
+    <div className="Page">
+      <h1 className="Page-Title">Behind the Scene Spoilers</h1>
+    </div>
+  );
+};
+
+export default BTSSpoilers;
