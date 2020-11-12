d79479e6c8c099d35b31cf207380480895114281
tweltar <sulatttweltarbo16@gmail.com>
2020-11-13 00:12:03 +0630

    Wishes Page - Add Jue's wish

--- src/pages/Wishes.js
+++ src/pages/Wishes.js
@@ -1,7 +1,135 @@
 import React from "react";
+import "../css/Wishes.css";
+import wish3 from "../assets/wishes/wish3.png";
+import wish1 from "../assets/wishes/wish1.png";
+import wish2 from "../assets/wishes/wish2.png";
 
 const Wishes = () => {
-  return <div>for you page</div>;
+  return (
+    <div>
+      <h1 className="Page-Title">BD Wishes</h1>
+      <div className="Jue">
+        <img src={wish3} className="Jue-paper" alt="" />
+        <p className="Jue-letter">
+          Dear Ausss, <br />
+          <br /> Your 20 journey begins now{" "}
+          <span style={{ fontSize: "110%" }}>&#128347;</span> Congratulations
+          !!!!! It's hard to believe you are already out of your teen years. We
+          spend 3/4 of our teenage years together. You have grown well and have
+          become a dutiful person both for your parents and for us . And I wish
+          that all of your goals, to be able to stay in a stress-free zone and
+          to always be beside us (dr ka tok nga atwt ngr su tg tr lol), will be
+          achieved. We had our ups and downs but we could pass well by caring
+          each other. You're the one who can tell my soul to, who can relate to
+          me like no one else, who can give energy in my downs, who can share a
+          lot of advices, who can know me better than I probably do. I literally
+          appreciate everything you do for me and I have learned a lot of tips
+          from you. You're the sweetest, most intelligent, most supportive, most
+          protective, and a mature girl for me.You ain't certainly patient for
+          everything but for me, you were. Do you know how special you are.
+          Thanks a million Aus. Never forget that I'm here for you and will
+          always be. We separated from each other for a half of the year. Right?
+          But, we can bond again without changing anything. And nothing will
+          ever change that. So many tears, jokes, memories, laughter, dreams,
+          lwal p yin lwal nay mhu myr, we've come a long way and have much
+          further to go. You literally mean so much to me. I love you w
+          countless of love ❤ <br />
+          <br /> P.S. Don't be sad so much. After this crisis, we gotta
+          celebrate your 20th birthday again! OMGGGGG!!!! I didn't know that I
+          could write these long and deep romantic letters for you ewww I can't
+          read it again :'' [I wanted to talk about these meaningful things but
+          I was shy lol] Ahh Btw baby gurl, go to the front of the mirror and
+          say "So what? We hot, we young" xD <br />
+          <br />{" "}
+          <span>
+            {" "}
+            From your beautiful baby gurl,
+            <br /> Juess
+          </span>
+        </p>
+      </div>
+      <div className="Yumi">
+        <img src={wish1} className="Yumi-paper" alt="" />
+        <p className="Yumi-letter">
+          Dear Ausss, <br />
+          <br /> Your 20 journey begins now{" "}
+          <span style={{ fontSize: "110%" }}>&#128347;</span> Congratulations
+          !!!!! It's hard to believe you are already out of your teen years. We
+          spend 3/4 of our teenage years together. You have grown well and have
+          become a dutiful person both for your parents and for us . And I wish
+          that all of your goals, to be able to stay in a stress-free zone and
+          to always be beside us (dr ka tok nga atwt ngr su tg tr lol), will be
+          achieved. We had our ups and downs but we could pass well by caring
+          each other. You're the one who can tell my soul to, who can relate to
+          me like no one else, who can give energy in my downs, who can share a
+          lot of advices, who can know me better than I probably do. I literally
+          appreciate everything you do for me and I have learned a lot of tips
+          from you. You're the sweetest, most intelligent, most supportive, most
+          protective, and a mature girl for me.You ain't certainly patient for
+          everything but for me, you were. Do you know how special you are.
+          Thanks a million Aus. Never forget that I'm here for you and will
+          always be. We separated from each other for a half of the year. Right?
+          But, we can bond again without changing anything. And nothing will
+          ever change that. So many tears, jokes, memories, laughter, dreams,
+          lwal p yin lwal nay mhu myr, we've come a long way and have much
+          further to go. You literally mean so much to me. I love you w
+          countless of love ❤ <br />
+          <br /> P.S. Don't be sad so much. After this crisis, we gotta
+          celebrate your 20th birthday again! OMGGGGG!!!! I didn't know that I
+          could write these long and deep romantic letters for you ewww I can't
+          read it again :'' [I wanted to talk about these meaningful things but
+          I was shy lol] Ahh Btw baby gurl, go to the front of the mirror and
+          say "So what? We hot, we young" xD <br />
+          <br />{" "}
+          <span>
+            {" "}
+            From your beautiful baby gurl,
+            <br /> Juess
+          </span>
+        </p>
+      </div>
+      <div className="Twel">
+        <img src={wish2} className="Twel-paper" alt="" />
+        <p className="Twel-letter">
+          Dear Ausss, <br />
+          <br /> Your 20 journey begins now{" "}
+          <span style={{ fontSize: "110%" }}>&#128347;</span> Congratulations
+          !!!!! It's hard to believe you are already out of your teen years. We
+          spend 3/4 of our teenage years together. You have grown well and have
+          become a dutiful person both for your parents and for us . And I wish
+          that all of your goals, to be able to stay in a stress-free zone and
+          to always be beside us (dr ka tok nga atwt ngr su tg tr lol), will be
+          achieved. We had our ups and downs but we could pass well by caring
+          each other. You're the one who can tell my soul to, who can relate to
+          me like no one else, who can give energy in my downs, who can share a
+          lot of advices, who can know me better than I probably do. I literally
+          appreciate everything you do for me and I have learned a lot of tips
+          from you. You're the sweetest, most intelligent, most supportive, most
+          protective, and a mature girl for me.You ain't certainly patient for
+          everything but for me, you were. Do you know how special you are.
+          Thanks a million Aus. Never forget that I'm here for you and will
+          always be. We separated from each other for a half of the year. Right?
+          But, we can bond again without changing anything. And nothing will
+          ever change that. So many tears, jokes, memories, laughter, dreams,
+          lwal p yin lwal nay mhu myr, we've come a long way and have much
+          further to go. You literally mean so much to me. I love you w
+          countless of love ❤ <br />
+          <br /> P.S. Don't be sad so much. After this crisis, we gotta
+          celebrate your 20th birthday again! OMGGGGG!!!! I didn't know that I
+          could write these long and deep romantic letters for you ewww I can't
+          read it again :'' [I wanted to talk about these meaningful things but
+          I was shy lol] Ahh Btw baby gurl, go to the front of the mirror and
+          say "So what? We hot, we young" xD <br />
+          <br />{" "}
+          <span>
+            {" "}
+            From your beautiful baby gurl,
+            <br /> Juess
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Wishes;
