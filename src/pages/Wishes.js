0c1c1818c1676340fd8df8996561dd01ec6d8237
tweltar <sulatttweltarbo16@gmail.com>
2020-11-13 14:58:57 +0630

    Add yumi's wish

--- src/pages/Wishes.js
+++ src/pages/Wishes.js
@@ -13,7 +13,8 @@ const Wishes = () => {
         <img src={wish3} className="Jue-paper" alt="" />
         <p className="Jue-letter">
           Dear Ausss, <br />
-          <br /> Your 20 journey begins now{" "}
+          <br />
+          Happy 20th Birthday! Your 20 journey begins now{" "}
           <span style={{ fontSize: "110%" }}>&#128347;</span> Congratulations
           !!!!! It's hard to believe you are already out of your teen years. We
           spend 3/4 of our teenage years together. You have grown well and have
@@ -41,92 +42,73 @@ const Wishes = () => {
           read it again :'' [I wanted to talk about these meaningful things but
           I was shy lol] Ahh Btw baby gurl, go to the front of the mirror and
           say "So what? We hot, we young" xD <br />
-          <br />{" "}
-          <span>
-            {" "}
-            From your beautiful baby gurl,
-            <br /> Juess
-          </span>
+          <br />
+          From your beautiful baby gurl,
+          <br /> Juess
         </p>
       </div>
       <div className="Yumi">
         <img src={wish1} className="Yumi-paper" alt="" />
         <p className="Yumi-letter">
-          Dear Ausss, <br />
-          <br /> Your 20 journey begins now{" "}
-          <span style={{ fontSize: "110%" }}>&#128347;</span> Congratulations
-          !!!!! It's hard to believe you are already out of your teen years. We
-          spend 3/4 of our teenage years together. You have grown well and have
-          become a dutiful person both for your parents and for us . And I wish
-          that all of your goals, to be able to stay in a stress-free zone and
-          to always be beside us (dr ka tok nga atwt ngr su tg tr lol), will be
-          achieved. We had our ups and downs but we could pass well by caring
-          each other. You're the one who can tell my soul to, who can relate to
-          me like no one else, who can give energy in my downs, who can share a
-          lot of advices, who can know me better than I probably do. I literally
-          appreciate everything you do for me and I have learned a lot of tips
-          from you. You're the sweetest, most intelligent, most supportive, most
-          protective, and a mature girl for me.You ain't certainly patient for
-          everything but for me, you were. Do you know how special you are.
-          Thanks a million Aus. Never forget that I'm here for you and will
-          always be. We separated from each other for a half of the year. Right?
-          But, we can bond again without changing anything. And nothing will
-          ever change that. So many tears, jokes, memories, laughter, dreams,
-          lwal p yin lwal nay mhu myr, we've come a long way and have much
-          further to go. You literally mean so much to me. I love you w
-          countless of love ❤ <br />
-          <br /> P.S. Don't be sad so much. After this crisis, we gotta
-          celebrate your 20th birthday again! OMGGGGG!!!! I didn't know that I
-          could write these long and deep romantic letters for you ewww I can't
-          read it again :'' [I wanted to talk about these meaningful things but
-          I was shy lol] Ahh Btw baby gurl, go to the front of the mirror and
-          say "So what? We hot, we young" xD <br />
-          <br />{" "}
-          <span>
-            {" "}
-            From your beautiful baby gurl,
-            <br /> Juess
-          </span>
+          Dear Au, <br />
+          <br /> Happy 20th birthday, Au
+          <span style={{ fontSize: "100%" }}> &#127874; </span> Yayyyyyy
+          CONGRATULATIONS, you have spent your teenage years well. I hope your
+          teenage days were filled with joy and love. And I wish for you to have
+          twenty-age days, free of sadness, hatred and every bad things.
+          <br />
+          <br /> Wow! You have grown into a beautiful young lady (I’m just
+          saying xD ) The world is so lucky to have someone like you, so are we.
+          Thank you so much for being my friend. It was such a gift to me, A
+          gift, no one but you can give. As I grow old, I found out that we
+          don’t choose our friends, they choose us. And you choose me.
+          <span style={{ fontSize: "100%" }}>❤️</span> I’m just so thankful.
+          <br />
+          <br />
+          People says 20th birthday is special as much as 18th because it is the
+          first year after teens and beginning of a new decade. I know it is
+          such a shame to spend this special day at home. But think about it,
+          you will have to spend your every birthday hanging out with us in the
+          future. So, this year has to be celebrated at home all day with your
+          family. Then, the rest of your 20 decade birthday have to be
+          CELEBRATED WITH US. So, don’t be too sad about it.
+          <br />
+          <br />
+          Well, Here it comes, birthday wishes. On this very special day, may
+          you be happy. May you have a wonderful decade. May you achieve all of
+          your goals in your near future. May you be able to chase your true
+          dream. And May you live “happily ever after”.
+          <br />
+          <br />
+          Since you’ve turned into 20, you are very officially an adult. You
+          will have to encounter more life struggle. I’ll be rooting for you.
+          And I’m always here to help you out. And lastly, I want you to know
+          that I love you <span style={{ fontSize: "110%" }}>
+            &#128584;
+          </span>{" "}
+          and I mean it. [ it’s not the gay me LOL, it’s straight me xD ]
+          <br />
+          <br />
+          Happy 20th birthday again.
+          <span style={{ fontSize: "110%" }}>❤️</span>
+          <br />
+          Have a wonderful day{" "}
+          <span style={{ fontSize: "110%" }}>&#128151;</span>
+          <br />
+          <br />
+          P.s My letter looks so random. Sorry :”) I wrote down what I could
+          think of when I can think of. Hehe. Whatever, enjoy{" "}
+          <span style={{ fontSize: "110%" }}>🤗</span>
+          <span style={{ fontSize: "110%" }}>&#128151;</span>
+          <br />
+          <br />- Sulei
         </p>
       </div>
       <div className="Twel">
         <img src={wish2} className="Twel-paper" alt="" />
         <p className="Twel-letter">
-          Dear Ausss, <br />
-          <br /> Your 20 journey begins now{" "}
-          <span style={{ fontSize: "110%" }}>&#128347;</span> Congratulations
-          !!!!! It's hard to believe you are already out of your teen years. We
-          spend 3/4 of our teenage years together. You have grown well and have
-          become a dutiful person both for your parents and for us . And I wish
-          that all of your goals, to be able to stay in a stress-free zone and
-          to always be beside us (dr ka tok nga atwt ngr su tg tr lol), will be
-          achieved. We had our ups and downs but we could pass well by caring
-          each other. You're the one who can tell my soul to, who can relate to
-          me like no one else, who can give energy in my downs, who can share a
-          lot of advices, who can know me better than I probably do. I literally
-          appreciate everything you do for me and I have learned a lot of tips
-          from you. You're the sweetest, most intelligent, most supportive, most
-          protective, and a mature girl for me.You ain't certainly patient for
-          everything but for me, you were. Do you know how special you are.
-          Thanks a million Aus. Never forget that I'm here for you and will
-          always be. We separated from each other for a half of the year. Right?
-          But, we can bond again without changing anything. And nothing will
-          ever change that. So many tears, jokes, memories, laughter, dreams,
-          lwal p yin lwal nay mhu myr, we've come a long way and have much
-          further to go. You literally mean so much to me. I love you w
-          countless of love ❤ <br />
-          <br /> P.S. Don't be sad so much. After this crisis, we gotta
-          celebrate your 20th birthday again! OMGGGGG!!!! I didn't know that I
-          could write these long and deep romantic letters for you ewww I can't
-          read it again :'' [I wanted to talk about these meaningful things but
-          I was shy lol] Ahh Btw baby gurl, go to the front of the mirror and
-          say "So what? We hot, we young" xD <br />
-          <br />{" "}
-          <span>
-            {" "}
-            From your beautiful baby gurl,
-            <br /> Juess
-          </span>
+          Dear Au, <br />
+          <br />
         </p>
       </div>
       <br />
