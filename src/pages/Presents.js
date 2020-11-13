383fac0f2f8e1a73efbef952a2f35b4befcefa6c
tweltar <sulatttweltarbo16@gmail.com>
2020-11-14 03:20:38 +0630

    Add Present Page

--- src/pages/Presents.js
+++ src/pages/Presents.js
@@ -1,9 +1,96 @@
 import React from "react";
+import present1 from "../assets/present/present1.PNG";
+import present2 from "../assets/present/present2.PNG";
 
 const Presents = () => {
   return (
-    <div>
+    <div className="Page">
       <h1 className="Page-Title">BD Presents for You</h1>
+      <p className="present-description">
+        Some surprises are still left hehe. You'll see now pr.
+        <br />
+        We sent birthday cakes to you. You love them, right? But, you might
+        wonder why the cake has a bite &#129300; Actually, it has a story lol.
+        Jue asked the bakery showing the photo just as in the screenshot. The
+        lady from the bakery misunderstood and made the cakes the exact same way
+        as in photo WITH THE BITE!!! &#129315;
+      </p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      ></div>
+      <br />
+      <p className="present-description">
+        Remember we sang a song for you during zoom meet-up? Here's the video if
+        you might want to see it again &#128157;
+      </p>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <iframe
+          className="video"
+          title="song"
+          width="966"
+          height="543"
+          src="https://www.youtube.com/embed/lMId45f5uRg"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <p className="present-description">
+        Sulei has a special surprise for youuu &#128149; She drew them
+        patiently. We hope you love them, toooo.
+      </p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+          alignItems: "center",
+          marginBottom: "50px",
+        }}
+      >
+        <img className="drawing-1" src={present1} alt="" />
+        <img className="drawing-2" src={present2} alt="" />
+      </div>
+      <p className="present-description">
+        Now, let's daydream for a bit &#129300; Shall we? What if Au were your
+        girlfriend? &#129325; Check out the video below xD
+      </p>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <iframe
+          className="video"
+          title="girlfriend"
+          width="966"
+          height="604"
+          src="https://www.youtube.com/embed/8RhxElpF5oQ"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <p className="present-description">
+        You really like Jeno so much. Well, imagine you were his girlfriend
+        &#128584; and watch these bonus videos for you. hehe
+      </p>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <iframe
+          className="video"
+          title="edit2"
+          width="966"
+          height="604"
+          src="https://www.youtube.com/embed/6eiyLxtWwJI"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+      https://youtu.be/tWtwoN6AVW0 Video edit 1
     </div>
   );
 };
